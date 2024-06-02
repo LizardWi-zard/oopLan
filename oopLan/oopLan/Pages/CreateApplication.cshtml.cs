58f1523e5525c099223ead0c601a76d99beac5f4
NicholasJKL <matveev_andrey2003@mail.ru>
2024-06-02 18:13:41 +0300

    Added base front

---
+++ oopLan/oopLan/Pages/CreateApplication.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace oopLan.Pages
+{
+    public class CreateApplicationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
